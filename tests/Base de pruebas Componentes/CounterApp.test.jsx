9e8631099f8012fbb2ec8a35e94f7d9265c15729
MatiasMeierCedeira <matias.meier@cedeira.com.ar>
2024-06-18 09:14:12 -0300

    update

--- tests/Base de pruebas Componentes/CounterApp.test.jsx
+++ tests/Base de pruebas Componentes/CounterApp.test.jsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../../src/CounterApp';
+import { getByText } from '@testing-library/dom';
 
 
 describe('Pruebas en el <CounterApp />', () => {
@@ -16,6 +17,33 @@ describe('Pruebas en el <CounterApp />', () => {
         render( <CounterApp value={ 100 } /> );
         expect( screen.getByText(100) ).toBeTruthy();
         expect( screen.getByRole('heading',{ level: 2}).innerHTML ).toContain('100')
+
+    });
+
+    test('debe incrementar con el boton +1', () => {
+        
+        render( <CounterApp value ={ initialValue } /> );
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText(11)).toBeTruthy();
+
+    });
+
+    test('debe disminuir con el boton -1', () => {
+        
+        render( <CounterApp value ={ initialValue } /> );
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText(9)).toBeTruthy();
+
+    });
+
+    test('debe funcionar el boton de reset', () => {
+        
+        render( <CounterApp value ={ initialValue } /> );
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByRole('button',{name:'btn-reset'}));
+        expect(screen.getByText(10)).toBeTruthy();
 
     });
 
